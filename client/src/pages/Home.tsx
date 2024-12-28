5cd593e354abeb38845ff86d72acd028298215c0
Lurldgbodex <professionaleditor6@gmail.com>
2024-12-28 18:40:59 +0100

    set up project for client side

---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,10 @@
+const Home = () => {
+    return (
+        <div className="min-h-screen flex flex-col items-center justify-center">
+          <h1 className="text-4xl font-bold bg-blue-500">Welcome to ShopSphere</h1>
+          <p className="mt-4 text-lg text-gray-600">Your one-stop e-commerce platform.</p>
+        </div>
+    );
+}
+
+export default Home;

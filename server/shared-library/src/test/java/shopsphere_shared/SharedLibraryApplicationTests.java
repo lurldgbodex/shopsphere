cab82adec7be21ebb25584f531b361069d415cfb
Lurldgbodex <professionaleditor6@gmail.com>
2025-01-02 16:13:14 +0100

    setup backend workspace

---
+++ server/shared-library/src/test/java/shopsphere_shared/SharedLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package shopsphere_shared;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharedLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

df8e1a5ffff40152511ab2199ebe44c50c9867fb
Lurldgbodex <professionaleditor6@gmail.com>
2025-01-03 20:45:30 +0100

    feat: add docker containerization and github workflow

---
+++ server/product-service/src/test/java/shopsphere_productservice/ProductServiceApplicationTest.java
@@ -0,0 +1,13 @@
+package shopsphere_productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ProductServiceApplicationTest {
+
+    @Test
+    void contextLoads() {}
+}

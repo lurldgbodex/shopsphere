626b5f05846240f1c2d52d1222e11222e1847a46
Lurldgbodex <professionaleditor6@gmail.com>
2025-01-10 18:37:15 +0100

    feat: added exception handling functionality

---
+++ server/product-service/src/main/java/shopsphere_productservice/exception/ProductExceptionHandler.java
@@ -0,0 +1,9 @@
+package shopsphere_productservice.exception;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import shopsphere_shared.handler.CustomExceptionHandler;
+
+@RestControllerAdvice
+public class ProductExceptionHandler extends CustomExceptionHandler {
+
+}

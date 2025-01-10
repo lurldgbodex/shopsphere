3360f415495590588e6a2ae027beb610df2f2aaa
Lurldgbodex <professionaleditor6@gmail.com>
2025-01-10 18:31:02 +0100

    feat: add function to verify user role

---
+++ server/shared-library/src/main/java/shopsphere_shared/Role.java
@@ -0,0 +1,7 @@
+package shopsphere_shared;
+
+public enum Role {
+    USER,
+    VENDOR,
+    ADMIN
+}
---
+++ server/shared-library/src/main/java/shopsphere_shared/utils/RoleUtils.java
@@ -0,0 +1,18 @@
+package shopsphere_shared.utils;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Component;
+import shopsphere_shared.Role;
+import shopsphere_shared.exceptions.ForbiddenException;
+
+@Component
+public class RoleUtils {
+    public void verifyRole(HttpHeaders headers, Role role) {
+        String userRole = headers.getFirst("X-User-Role");
+        String requiredRole = role.toString();
+
+        if (!requiredRole.equalsIgnoreCase(userRole)) {
+            throw new ForbiddenException("Access Denied");
+        }
+    }
+}

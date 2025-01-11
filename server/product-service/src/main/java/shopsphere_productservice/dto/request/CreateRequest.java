205e2a9697ad41804f5143d61fa1118404a6070a
Lurldgbodex <professionaleditor6@gmail.com>
2025-01-11 22:20:09 +0100

    chore: add constructors

--- server/product-service/src/main/java/shopsphere_productservice/dto/request/CreateRequest.java
+++ server/product-service/src/main/java/shopsphere_productservice/dto/request/CreateRequest.java
@@ -3,12 +3,17 @@ package shopsphere_productservice.dto.request;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class CreateRequest {
     @NotBlank(message = "name is required")
     private String name;

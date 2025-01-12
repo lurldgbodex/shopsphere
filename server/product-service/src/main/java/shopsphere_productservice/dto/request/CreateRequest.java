cc7579a711895594f90aaff888576ae7b47287e6
Lurldgbodex <professionaleditor6@gmail.com>
2025-01-12 14:42:02 +0100

    chore: add unit test for vendor product controller

--- server/product-service/src/main/java/shopsphere_productservice/dto/request/CreateRequest.java
+++ server/product-service/src/main/java/shopsphere_productservice/dto/request/CreateRequest.java
@@ -18,7 +18,7 @@ public class CreateRequest {
     @NotBlank(message = "name is required")
     private String name;
     @NotNull(message = "quantity is required")
-    @Min(value = 1, message = "quantity must be more that 0")
+    @Min(value = 1, message = "quantity must be more than 0")
     private int quantity;
     @NotNull(message = "price is required")
     private double price;
@@ -26,6 +26,6 @@ public class CreateRequest {
     private String category;
     @NotBlank(message = "description is required")
     private String description;
-    @NotBlank(message = "images is required")
+    @NotNull(message = "images is required")
     private List<String> images;
 }

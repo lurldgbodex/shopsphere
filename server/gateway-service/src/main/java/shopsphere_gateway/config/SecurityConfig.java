4114bb2c9255a7a76cc5d898c65b4bc606f496e6
Lurldgbodex <professionaleditor6@gmail.com>
2025-01-24 19:26:33 +0100

    remove oauth2 from filter chain config

--- server/gateway-service/src/main/java/shopsphere_gateway/config/SecurityConfig.java
+++ server/gateway-service/src/main/java/shopsphere_gateway/config/SecurityConfig.java
@@ -2,7 +2,6 @@ package shopsphere_gateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
@@ -26,7 +25,6 @@ public class SecurityConfig {
                         .pathMatchers("/eureka/**").hasRole("ADMIN")
                         .pathMatchers("/api/docs/**", "/docs/**", "/swagger-ui/**").permitAll()
                         .anyExchange().authenticated())
-                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
                 .build();
     }
 

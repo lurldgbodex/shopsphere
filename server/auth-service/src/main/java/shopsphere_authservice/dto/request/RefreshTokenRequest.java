2191ac96b2fd07eed19c94f39fe07e346546d3e6
Lurldgbodex <professionaleditor6@gmail.com>
2025-01-27 20:48:02 +0100

    fix: handle request validation

--- server/auth-service/src/main/java/shopsphere_authservice/dto/request/RefreshTokenRequest.java
+++ server/auth-service/src/main/java/shopsphere_authservice/dto/request/RefreshTokenRequest.java
@@ -1,8 +1,10 @@
 package shopsphere_authservice.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class RefreshTokenRequest {
+    @NotBlank(message = "token is required")
     private String token;
 }
